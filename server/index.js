d38c3d6fca1c76e444e49be19ac5140e106a9b57
PaolaP <paolaperez8@gmail.com>
2021-12-09 15:49:08 -0500

    custom fonts added

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
+/** @format */
+
 "use strict";
 
 // Basic express setup:
 
-const PORT          = 8080;
-const express       = require("express");
-const bodyParser    = require("body-parser");
-const app           = express();
+const PORT = 8080;
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -33,5 +35,5 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 app.use("/tweets", tweetsRoutes);
 
 app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
+	console.log("Example app listening on port " + PORT);
 });
