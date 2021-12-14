4ed1ba7f0f397d9579bc1855c5d61bf6e8cb46e8
PaolaP <paolaperez8@gmail.com>
2021-12-13 23:08:57 -0500

    character count wrks also red in negative numbers

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,28 @@
 $(document).ready(function() {
-  console.log("test works");
-function logKey(e) {
-      console.log(` ${e.code}`);
-    };
-const textarea = document.querySelector("#tweet-text");
-   textarea.addEventListener('keydown', logKey);
+
+  $("#tweet-text").on('input', function(){
+//console.log(this);
+const $charLength = $(this).val().length;
+const $charLeft = 140 - $charLength;
+console.log($charLength);
+//$("#counter")
+
+//TODO fix once reach limit fo negtive numbers it all turns red
+//take css to css place///how to do that add html new class ?
+//make sure siblings is the right way to go.. also input vs keydown
+console.log(count);
+if ($charLeft > 0){
+let count = $(this).siblings("output");
+count.text($charLeft);
+} else {
+  let count = $(this).siblings("output").css( "color", "red" );
+count.text($charLeft);
+}
+
+  });
+// function logKey(e) {
+//       console.log(` ${e.code}`);
+//     };
+// const textarea = document.querySelector("#tweet-text");
+//    textarea.addEventListener('keydown', logKey);
 });
