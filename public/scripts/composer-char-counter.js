e356511336153a537369a8640ca567cd75e3c4c2
PaolaP <paolaperez8@gmail.com>
2021-12-13 21:41:17 -0500

    char-counter.js uses eventlistener to log keystroke event

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-  // --- our code goes here ---
+  console.log("test works");
+function logKey(e) {
+      console.log(` ${e.code}`);
+    };
+const textarea = document.querySelector("#tweet-text");
+   textarea.addEventListener('keydown', logKey);
 });
