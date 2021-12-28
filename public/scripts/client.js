68475c0c33ab7b7e71f484bf2362fbbace3afdd1
PaolaP <paolaperez8@gmail.com>
2021-12-27 19:01:08 -0500

    escape function to text input by user

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,25 +1,34 @@
-/*
+/**
+ * /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+ *
+ * @format
  */
 
 // Test / driver code (temporary). Eventually will get this from the server.
 
-$(document).ready(function(){
+$(document).ready(function () {
+	//avoid XSS attacks on string literals
+	const escape = function (str) {
+		let div = document.createElement("div");
+		div.appendChild(document.createTextNode(str));
+		return div.innerHTML;
+	};
 
-//create new DOM element and appends it 
-const renderTweets = function(tweets){
-//(doesn't reload all tweets
-   $("#tweet-container").empty();
-  for (const tweet of tweets){
-    let $renderTweet = createTweetElement(tweet);
-   $("#tweet-container").prepend($renderTweet);
-  }
-};
+	//create new DOM element and appends it
+	const renderTweets = function (tweets) {
+		//(doesn't reload all tweets
+		$("#tweet-container").empty();
+		for (const tweet of tweets) {
+			let $renderTweet = createTweetElement(tweet);
+			$("#tweet-container").prepend($renderTweet);
+		}
+	};
 
-const createTweetElement = function(tweet){
-  const $tweet = $(`
+	const createTweetElement = function (tweet) {
+		const $tweet = $(`
     	<article class="tweet">
 				<header class="user">
 <div class="user-picture-name">
@@ -35,7 +44,7 @@ const createTweetElement = function(tweet){
 </div>
 				</header>
 				<p class="tweets-posted">
-					${tweet.content.text}
+					${escape(tweet.content.text)}
 				</p>
 				<hr>
 				<footer>
@@ -49,42 +58,37 @@ const createTweetElement = function(tweet){
 
 					</span>
 				</footer>
-			</article>`
-  );
-  return $tweet;
-}
+			</article>`);
+		return $tweet;
+	};
 
+	$("#tweet-form").on("submit", (event) => {
+		event.preventDefault();
+		//validate length of tweet and alert error
+		let lengthTweet = $("#tweet-text").val().length;
+		if (lengthTweet > 140) {
+			alert("Only 140 characters allowed");
+			return;
+		} else if (!lengthTweet) {
+			alert("write a tweet");
+		}
 
+		//serialize and post tweet
+		let $tweet = $("#tweet-form").serialize();
+		$.post("/tweets/", $tweet, (err, data) => {
+			loadTweets();
 
-$("#tweet-form").on("submit", event =>{
-  event.preventDefault();
-//validate length of tweet and alert error
-  let lengthTweet = $("#tweet-text").val().length;
-  if (lengthTweet >140){
-    alert("Only 140 characters allowed");
-    return;
-  }else if(!lengthTweet){
-    alert("write a tweet")
-  };
- 
-//serialize and post tweet
-  let $tweet = $("#tweet-form").serialize();
-  $.post("/tweets/", $tweet, (err, data) => {
-    loadTweets();
+			//clears text box after post
+			const $input = $("#tweet-text");
+			$input.val(" ").focus();
+		});
+	});
 
-//clears text box after post
-    const $input = $("#tweet-text");
-    $input.val(" ").focus();
-  })
-})
-
-
-//loads tweets
-const loadTweets = () => {
-  $.get("/tweets", (tweet) => {
-    renderTweets(tweet);
-  })
-}
-loadTweets();
+	//loads tweets
+	const loadTweets = () => {
+		$.get("/tweets", (tweet) => {
+			renderTweets(tweet);
+		});
+	};
+	loadTweets();
 });
-
