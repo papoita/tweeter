5c676e89e4d0b182045d2e10b20ce23054105587
PaolaP <paolaperez8@gmail.com>
2022-01-05 19:28:13 -0500

    stretch buttons using html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,26 +1,26 @@
 
-$(document).ready(function () {
+$(document).ready(function() {
   $(".error").hide();
 
-	const escape = function (str) {
-		const div = document.createElement("div");
-		div.appendChild(document.createTextNode(str));
-		return div.innerHTML;
-	};
+  const escape = function(str) {
+    const div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
-	const renderTweets = function (tweets) {
-		$("#tweets-container").empty();
-		for (const tweet of tweets) {
-			const $renderTweet = createTweetElement(tweet);
-			$("#tweets-container").prepend($renderTweet);
-		}
-	};
+  const renderTweets = function(tweets) {
+    $("#tweets-container").empty();
+    for (const tweet of tweets) {
+      const $renderTweet = createTweetElement(tweet);
+      $("#tweets-container").prepend($renderTweet);
+    }
+  };
 
-	const createTweetElement = function (tweet) {
-		const { content, user, created_at } = tweet;
-		const { avatars, handle, name } = user;
+  const createTweetElement = function(tweet) {
+    const { content, user, created_at } = tweet;
+    const { avatars, handle, name } = user;
 
-		return `
+    return `
     	<article class="tweet">
 				<header class="user">
 					<div class="user-picture-name">
@@ -51,46 +51,46 @@ $(document).ready(function () {
 				</footer>
 			</article>
 		`;
-	};
+  };
 
-  const errorDisplay = function (errorText){
-		const error = $(".error");
+  const errorDisplay = function(errorText) {
+    const error = $(".error");
 
-		error.slideDown("slow").text(errorText);
+    error.slideDown("slow").text(errorText);
 
-		setTimeout(()=>{
-			error.hide(300);
-		}, 3000);
-	};
+    setTimeout(()=>{
+      error.hide(300);
+    }, 3000);
+  };
 
-	$("#tweet-form").on("submit", (event) => {
-		event.preventDefault();
+  $("#tweet-form").on("submit", (event) => {
+    event.preventDefault();
 
-		const lengthTweet = $("#tweet-text").val().length;
+    const lengthTweet = $("#tweet-text").val().length;
 
-		if (lengthTweet > 140) {
-			errorDisplay("🛑 TOO LONG! Only 140 characters allowed 🛑");
-		}
+    if (lengthTweet > 140) {
+      errorDisplay("🛑 TOO LONG! Only 140 characters allowed 🛑");
+    }
 
-		if (!lengthTweet) {
-			errorDisplay("🛑 TOO SHORT! What are you really thinking? 🛑 ");
-		}
+    if (!lengthTweet) {
+      errorDisplay("🛑 TOO SHORT! What are you really thinking? 🛑 ");
+    }
 
-		let $tweet = $("#tweet-form").serialize();
-		$.post("/tweets/", $tweet, (err, data) => {
-			loadTweets();
+    let $tweet = $("#tweet-form").serialize();
+    $.post("/tweets/", $tweet, (err, data) => {
+      loadTweets();
 
-			const $input = $("#tweet-text");
-			$input.val(" ").focus();
-			$(".counter").html("140");
-		});
-	});
+      const $input = $("#tweet-text");
+      $input.val(" ").focus();
+      $(".counter").html("140");
+    });
+  });
 
-	//loads tweets
-	const loadTweets = () => {
-		$.get("/tweets", (tweet) => {
-			renderTweets(tweet);
-		});
-	};
-	loadTweets();
+  //loads tweets
+  const loadTweets = () => {
+    $.get("/tweets", (tweet) => {
+      renderTweets(tweet);
+    });
+  };
+  loadTweets();
 });
