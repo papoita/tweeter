25f3e93ba6ef3d4e479213d4199c6a5c2973160a
PaolaP <paolaperez8@gmail.com>
2022-01-05 23:27:16 -0500

    dry code fixed indentations

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,25 +1,17 @@
-
 $(document).ready(function() {
-  $(".error").hide();
+  $("#new-tweet-error").hide();
 
-  const escape = function (str) {
-		const div = document.createElement("div");
-		div.appendChild(document.createTextNode(str));
-		return div.innerHTML;
-	};
-
-	$(".write-new-tweet, .btn-top-page").click(function(e){
-		e.preventDefault();
-		//$("body").scrollTop()
+	$("#write-new-tweet, #btn-top-page").click(function(event){
+		event.preventDefault();
 		$("html, body").animate({ scrollTop: "0" })
-		$("#tweet-text").focus();
+		$("#new-tweet-input").focus();
 	});
 
   const renderTweets = function(tweets) {
     $("#tweets-container").empty();
     for (const tweet of tweets) {
-      const renderTweet = createTweetElement(tweet);
-      $("#tweets-container").prepend(renderTweet);
+      const renderedTweet = createTweetElement(tweet);
+      $("#tweets-container").prepend(renderedTweet);
     }
   };
 
@@ -27,37 +19,41 @@ $(document).ready(function() {
     const { content, user, created_at } = tweet;
     const { avatars, handle, name } = user;
 
+    const escape = function (str) {
+      const div = document.createElement("div");
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML;
+	  };
+
     return `
     	<article class="tweet">
-				<header class="user">
-					<div class="user-picture-name">
-						<img class="user-icon" src="${avatars}"></img>
-						<p class="username">
-							${name}
-						</p>
+				<header class="tweet-header">
+					<div class="tweet-header-user">
+						<img src="${avatars}" alt="tweet user avatar"></img>
+						<p>${name}</p>
 					</div>
-					<h2 class="user-handle">${handle}</h2>
+					<h2 class="tweet-header-handle">${handle}</h2>
 				</header>
-				<span class="tweets-posted">
+				<p class="tweet-content">
 					${escape(content.text)}
-				</span>
+				</p>
 				<hr>
-				<footer>
-					<span class="time-passed">
+				<footer class="tweet-footer">
+					<p class="tweet-footer-timestamp">
 					${timeago.format(created_at)}
-					</span>
-					<span class="tweet-icons">
+					</p>
+					<div class="tweet-footer-icons">
 						<i class="far fa-flag"></i>
 						<i class="fas fa-retweet"></i>
 						<i class="far fa-heart"></i>
-					</span>
+					</div>
 				</footer>
 			</article>
 		`;
   };
 
   const errorDisplay = function(errorText) {
-    const error = $(".error");
+    const error = $("#new-tweet-error");
 
     error.slideDown("slow").text(errorText);
 
@@ -66,10 +62,10 @@ $(document).ready(function() {
     }, 3000);
   };
 
-  $("#tweet-form").on("submit", (event) => {
+  $("#new-tweet-form").on("submit", function (event) {
     event.preventDefault();
 
-    const lengthTweet = $("#tweet-text").val().length;
+    const lengthTweet = $("#new-tweet-input").val().length;
 
     if (!lengthTweet) {
       return errorDisplay("🛑 TOO SHORT! What are you really thinking? 🛑 ");
@@ -79,13 +75,13 @@ $(document).ready(function() {
       return errorDisplay("🛑 TOO LONG! Only 140 characters allowed 🛑");
     }
 
-    let $tweet = $("#tweet-form").serialize();
-    $.post("/tweets/", $tweet, (err, data) => {
+    let tweet = $(this).serialize();
+    $.post("/tweets/", tweet, (err, data) => {
       loadTweets();
 
-      const $input = $("#tweet-text");
-      $input.val(" ").focus();
-      $(".counter").html("140");
+      const input = $("#new-tweet-input");
+      input.val("").focus();
+      $("#new-tweet-counter").html("140");
     });
   });
 
@@ -95,5 +91,6 @@ $(document).ready(function() {
       renderTweets(tweet);
     });
   };
+  
   loadTweets();
 });
